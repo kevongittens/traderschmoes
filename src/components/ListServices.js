4767c81cee5d9bf75019fcc4a9fa6b0518e082d3
kevongittens <kevon.gittens@yahoo.com>
2021-12-26 21:08:41 -0500

    +List component

---
+++ src/components/ListServices.js
@@ -0,0 +1,18 @@
+import React from "react";
+//import reactDom from "react-dom";
+
+//i/mport { Auth } from "aws-amplify";
+//import awsconfig from '../aws-exports';
+//import { withAuthenticator } from 'aws-amplify-react';
+////Auth.configure(awsconfig);
+
+const ListServices = () => {
+
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default ListServices;
